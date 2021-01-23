861cb6250b1a15206e08ca9fcbf35e4bc3409db1
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-23 14:47:03 -0700

    Added urls.py to landingpage

--- adventure_x/goal1/views.py
+++ adventure_x/goal1/views.py
@@ -2,4 +2,4 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello world testy  tst")
+    return HttpResponse("At goal1 index")
