3b1739f50f6fd256ee6b7e4c4bff7146f4298c7d
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-23 15:49:42 -0700

    added index and base html files

--- adventure_x/landingpage/models.py
+++ adventure_x/landingpage/models.py
@@ -4,3 +4,5 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     karma = models.IntegerField(default=0)
     comrades = models.IntegerField(default=10)
+    def __str__(self):
+        return self.name + " has " + str(self.karma) + " karma, and " + str(self.comrades) + " friends!"
--- adventure_x/landingpage/views.py
+++ adventure_x/landingpage/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render('index.html')
+    context = {}
+    
+    return render(request, 'index.html', context)
 
 def startgame(request):
     return HttpResponse("You have started the game")
