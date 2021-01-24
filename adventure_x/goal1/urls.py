a2e7dcd63b708d9c677133be9ed3d11a9cb34dfc
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-23 17:08:39 -0700

    added the question pages with required html and basic views

--- adventure_x/goal1/urls.py
+++ adventure_x/goal1/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('q2/', views.q2, name='q2'),
+    path('q3/', views.q3, name='q3'),
 ]
--- adventure_x/goal1/views.py
+++ adventure_x/goal1/views.py
@@ -2,4 +2,10 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("At goal1 index test")
+    return render(request, 'index.html', {})
+
+def q2(request):
+    return render(request, 'q2.html', {})
+
+def q3(request):
+    return render(request, 'q3.html', {})
