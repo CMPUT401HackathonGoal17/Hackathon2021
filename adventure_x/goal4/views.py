5f64abd925063d80ae9e610b508f15db92ab7219
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-24 12:03:16 -0700

    renamed files causing issues to their goal names

--- adventure_x/goal4/views.py
+++ adventure_x/goal4/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 def q1(request):
     context = {} 
-    return render(request, 'q1.html', context)
+    return render(request, 'g4q1.html', context)
 
 def q2(request):
     context = {} 
-    return render(request, 'q2.html', context)
+    return render(request, 'g4q2.html', context)
 
 def q3(request):
     context = {} 
-    return render(request, 'q3.html', context)
+    return render(request, 'g4q3.html', context)
