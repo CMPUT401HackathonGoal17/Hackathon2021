3f329c835db75cd5344fa960ca13c224db24f73e
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-24 11:19:36 -0700

    fixed db, and form is now working for landing page

--- adventure_x/landingpage/views.py
+++ adventure_x/landingpage/views.py
@@ -1,16 +1,25 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from .forms import UserForm
 def index(request):
     if request.method == "POST":
         form = UserForm(request.POST or None)
+        print(form)
+        print(form.is_valid())
+        print(form.errors)
         if form.is_valid():
             form.save()
-    
-    context = {}
-    return render(request, 'index.html', context)
+        return redirect('startgame')
+    else:
+        context = {}
+        return render(request, 'index.html', context)
 
 def startgame(request):
-    user = User.objects.latest('date_added')
-    return render(request, 'startgame.html', {"user":user})
+    users = User.objects.order_by('id')
+    print(users)
+    if users.count() >= 1:
+        user = users.last() # get the latest user
+        return render(request, 'startgame.html', {"user":user.name})
+    else:
+        return render(request, 'startgame.html', {"user":"Guest"})
