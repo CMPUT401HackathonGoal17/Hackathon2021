fc207750b97932c4595d26ce97bff411deb00ad0
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-01-24 09:44:59 -0700

    added startgame

--- adventure_x/landingpage/views.py
+++ adventure_x/landingpage/views.py
@@ -1,10 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from .models import User
+from .forms import UserForm
 def index(request):
-    context = {}
+    if request.method == "POST":
+        form = UserForm(request.POST or None)
+        if form.is_valid():
+            form.save()
     
+    context = {}
     return render(request, 'index.html', context)
 
 def startgame(request):
-    return HttpResponse("You have started the game")
+    user = User.objects.latest('date_added')
+    return render(request, 'startgame.html', {"user":user})
